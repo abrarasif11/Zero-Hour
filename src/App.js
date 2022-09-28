696e2d72fc13d5fc7aa028f1a7786f237d237422
abrarasif11 <faasif929@gmail.com>
2022-09-28 22:33:25 +0600

    header-part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './component/Header/Header';
 
 function App() {
   return (
+    <div className='body'>
     <div className="App">
-      
+      <Header></Header>
+    </div>
     </div>
   );
 }
