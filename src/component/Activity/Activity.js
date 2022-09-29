b919958c5e4655809857ae2b8dfa0d9972e2fb58
abrarasif11 <faasif929@gmail.com>
2022-09-29 15:49:28 +0600

    added-header-icon

--- src/component/Activity/Activity.js
+++ src/component/Activity/Activity.js
@@ -23,6 +23,7 @@ const Activity = () => {
             <div className="profile-container">
                <img src="" alt="" />
                <h4 className='profile-text'>Pintu Islam</h4>
+               <h6 class="text-center">Student</h6>
                <p className='profile-text'>Location : Dhaka,Bangladesh</p>
                <div className='info'>
                 <h6 className='weight'>80KG Weight</h6>
@@ -33,10 +34,10 @@ const Activity = () => {
                </div>
                 <h5 className='break-text'>Add a Break</h5>
                <div className='break'>
-                <button className='s10'>10s</button>
-                <button className='s20'>20s</button>
-                <button className='s30'>30s</button>
-                <button className='s40'>40s</button>
+                <button className='s10'>10m</button>
+                <button className='s20'>20m</button>
+                <button className='s30'>30m</button>
+                <button className='s40'>40m</button>
                </div>
                <h5 class='text-center mt-5 pt-4 mb-4'>Activity Details</h5>
                <div>
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../../images/img_410627.png';
 
 import './Header.css';
 
@@ -6,8 +7,8 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header ps-5 pt-2 pb-2'>
-            <h2 className='header-text'>Zero Hour-Active-Club</h2>
-            <h6>Select today’s Activities</h6>
+            <h2 className='header-text'> <img className='icon' src={logo} alt="" /> Zero Hour-Active-Club</h2>
+            <h6 class="ms-5">Select today’s Activities</h6>
         </div>
         
     );
