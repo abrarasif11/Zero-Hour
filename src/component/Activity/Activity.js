06b3cde8b84ef1cabd98a828f632f460f84a5b3b
abrarasif11 <faasif929@gmail.com>
2022-09-29 21:47:44 +0600

    added-time

--- src/component/Activity/Activity.js
+++ src/component/Activity/Activity.js
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Activities from '../Activities/Activities';
+import Profile from '../Header/Profile/Profile';
 import './Activity.css';
 
 const Activity = () => {
     const [activities, setActivities] = useState([]);
-    const [times, setTimes] = useState([]);
+    
 
     useEffect(()=>{
         fetch('activity.json')
         .then(res=> res.json())
         .then(data => setActivities(data))
-    },[])
-    let time = 0
-    for(const activity of activities){
-         time = time + activity.time;  
-    }
+    },[]);
+    const [activeObj, setActiveObj] = useState([]);
     const handleClick = (activity) =>{
-        console.log(activity);
-        const newTime = [...times, activity]
-        setTimes(newTime);
+        // console.log(activity);
+        const newTime = [...activeObj, activity]
+        setActiveObj(newTime);
       }
     return (
         <div className='main-container'>
@@ -32,30 +30,7 @@ const Activity = () => {
                 }
             </div>
             <div className="profile-container">
-            <h4 className='profile-text'>Pintu Islam</h4>
-               <h6 class="text-center">Student</h6>
-               <p className='profile-text'>Location : Dhaka,Bangladesh</p>
-               <div className='info'>
-                <h6 className='weight'>80KG Weight</h6>
-                <h6 className='height'>6.1 Height</h6>
-                
-                <h6 className='age'>21Years Age</h6>
-                
-               </div>
-                <h5 className='break-text'>Add a Break</h5>
-               <div className='break'>
-                <button className='s10'>10m</button>
-                <button className='s20'>20m</button>
-                <button className='s30'>30m</button>
-                <button className='s40'>40m</button>
-               </div>
-               <h5 class='text-center mt-5 pt-4 mb-4'>Activity Details</h5>
-               <div>
-
-               <h6 className='activity-time'>Activity Time :  minitues</h6>
-               <h6 className='break-time'>Break Time : 00 Seconds</h6>
-               </div>
-               <button className='activity-btn'>Activity Completed</button>
+            <Profile activeObj={activeObj}></Profile>
             </div>
         </div>
     );
--- src/component/Header/Profile/Profile.js
+++ src/component/Header/Profile/Profile.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
-const Profile = () => {
-    
+const Profile = (props) => {
+    const  {activeObj} = props
+    let totalTime = 0;
+    for(const activity of activeObj){
+        // console.log(time);
+        totalTime = totalTime + activity.time;
+    } 
     return (
         <div >
             
-           
-            </div>
+            <h4 className='profile-text'>Pintu Islam</h4>
+               <h6 class="text-center">Student</h6>
+               <p className='profile-text'>Location : Dhaka,Bangladesh</p>
+               <div className='info'>
+                <h6 className='weight'>80KG Weight</h6>
+                <h6 className='height'>6.1 Height</h6>
+                
+                <h6 className='age'>21Years Age</h6>
+                
+               </div>
+                <h5 className='break-text'>Add a Break</h5>
+               <div className='break'>
+                <button className='s10'>10m</button>
+                <button className='s20'>20m</button>
+                <button className='s30'>30m</button>
+                <button className='s40'>40m</button>
+               </div>
+               <h5 class='text-center mt-5 pt-4 mb-4'>Activity Details</h5>
+               <div>
+
+               <h6 className='activity-time'>Activity Time : {totalTime} Minitues</h6>
+               <h6 className='break-time'>Break Time :  Minitues</h6>
+               </div>
+               <button className='activity-btn'>Activity Completed</button>
+        </div>
             
         
     );
