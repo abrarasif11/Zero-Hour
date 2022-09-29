93f20836d69687adacbbf42a03bf2f327eedf99e
abrarasif11 <faasif929@gmail.com>
2022-09-29 09:14:16 +0600

    profile-site-done

--- src/component/Activity/Activity.js
+++ src/component/Activity/Activity.js
@@ -32,6 +32,20 @@ const Activity = () => {
                 <h6 className='age'>21 Years</h6>
                 <p >Age</p>
                </div>
+                <h5 className='break-text'>Add a Break</h5>
+               <div className='break'>
+                <button className='s10'>10s</button>
+                <button className='s20'>20s</button>
+                <button className='s30'>30s</button>
+                <button className='s40'>40s</button>
+               </div>
+               <h5 class='text-center mt-5 pt-4 mb-4'>Activity Details</h5>
+               <div>
+
+               <h6 className='activity-time'>Activity Time : 00Seconds</h6>
+               <h6 className='break-time'>Break Time : 00Seconds</h6>
+               </div>
+               <button className='activity-btn'>Activity Completed</button>
             </div>
         </div>
     );
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <div className='header ps-5 pt-2 pb-2'>
             <h2 className='header-text'>Zero Hour-Active-Club</h2>
-            <h6>Select today’s exercise</h6>
+            <h6>Select today’s Activities</h6>
         </div>
         
     );
