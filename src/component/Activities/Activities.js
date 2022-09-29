d6d6a7ed72ea5ca272b93cb4e24d4621c6ea5ca4
abrarasif11 <faasif929@gmail.com>
2022-09-29 07:17:25 +0600

    display-activities-3-columns

--- src/component/Activities/Activities.js
+++ src/component/Activities/Activities.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Activities.css';
 
 const Activities = (props) => {
     const {picture, time, name, age} = props.activity
     return (
-        <div>
+        <div className='activities'>
             <img src={picture} alt="" />
             <h4>{name}</h4>
             <p>Age : {age}</p>
